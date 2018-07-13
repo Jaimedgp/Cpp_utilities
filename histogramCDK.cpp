99a851bd88ba5adc0b1a1b647f097e2ff8192b8e
Jaime <jaime.diez.gp@gmail.com>
2018-07-13 10:49:00 +0200

    Playing and understanding the cdk histogram

--- histogramCDK.cpp
+++ histogramCDK.cpp
@@ -14,75 +14,32 @@ int main (int argc, char **argv)
 {
    /* *INDENT-EQLS* */
    CDKSCREEN *cdkscreen         = 0;
-   CDKHISTOGRAM *volume         = 0;
-   CDKHISTOGRAM *bass           = 0;
    CDKHISTOGRAM *treble         = 0;
-   const char *volumeTitle      = "<C></5>Volume<!5>";
-   const char *bassTitle        = "<C></5>Bass  <!5>";
+   CDKHISTOGRAM *Other         = 0;
    const char *trebleTitle      = "<C></5>Treble<!5>";
 
    CDK_PARAMS params;
    boolean Box;
 
    CDKparseParams (argc, argv, &params, CDK_CLI_PARAMS);
-   Box = CDKparamValue (&params, 'N', TRUE);
+   Box = CDKparamValue (&params, 'N', FALSE);
 
    cdkscreen = initCDKScreen (NULL);
 
    /* Start CDK Color. */
    initCDKColor ();
 
-   /* Create the histogram objects. */
-   volume = newCDKHistogram (cdkscreen,
-			     CDKparamValue (&params, 'X', 10),
-			     CDKparamValue (&params, 'Y', 10),
-			     CDKparamValue (&params, 'H', 1),
-			     CDKparamValue (&params, 'W', -2),
-			     HORIZONTAL, volumeTitle,
-			     Box,
-			     CDKparamValue (&params, 'S', FALSE));
-   if (volume == 0)
-   {
-      /* Exit CDK. */
-      destroyCDKScreen (cdkscreen);
-      endCDK ();
-
-      printf ("Cannot make volume histogram. Is the window big enough??\n");
-      //ExitProgram (EXIT_FAILURE);
-   }
-
-   bass = newCDKHistogram (cdkscreen,
-			   CDKparamValue (&params, 'X', 10),
-			   CDKparamValue (&params, 'Y', 14),
-			   CDKparamValue (&params, 'H', 1),
-			   CDKparamValue (&params, 'W', -2),
-			   HORIZONTAL, bassTitle,
-			   Box,
-			   CDKparamValue (&params, 'S', FALSE));
-   if (bass == 0)
-   {
-      /* Exit CDK. */
-      destroyCDKHistogram (volume);
-      destroyCDKScreen (cdkscreen);
-      endCDK ();
-
-      printf ("Cannot make bass histogram. Is the window big enough??\n");
-      //ExitProgram (EXIT_FAILURE);
-   }
-
    treble = newCDKHistogram (cdkscreen,
-			     CDKparamValue (&params, 'X', 10),
-			     CDKparamValue (&params, 'Y', 18),
-			     CDKparamValue (&params, 'H', 1),
-			     CDKparamValue (&params, 'W', -2),
-			     HORIZONTAL, trebleTitle,
+			     CDKparamValue (&params, 'X', 8), // position of leftup corner X
+			     CDKparamValue (&params, 'Y', 1), // position of left corner Y
+			     CDKparamValue (&params, 'H', 20), // height of the column
+			     CDKparamValue (&params, 'W', 10), // width of the column
+			     VERTICAL, "0",
 			     Box,
-			     CDKparamValue (&params, 'S', FALSE));
+			     CDKparamValue (&params, 'T', FALSE));
    if (treble == 0)
    {
       /* Exit CDK. */
-      destroyCDKHistogram (volume);
-      destroyCDKHistogram (bass);
       destroyCDKScreen (cdkscreen);
       endCDK ();
 
@@ -90,53 +47,63 @@ int main (int argc, char **argv)
       //ExitProgram (EXIT_FAILURE);
    }
 
+   Other = newCDKHistogram (cdkscreen,
+			     CDKparamValue (&params, 'X', 20), // position of leftup corner X
+			     CDKparamValue (&params, 'Y', 1), // position of left corner Y
+			     CDKparamValue (&params, 'H', 20), // height of the column
+			     CDKparamValue (&params, 'W', 10), // width of the column
+			     VERTICAL, "0.6",
+			     Box,
+			     CDKparamValue (&params, 'T', FALSE));
+   if (Other == 0)
+   {
+      /* Exit CDK. */
+      destroyCDKScreen (cdkscreen);
+      endCDK ();
+
+      printf ("Cannot make treble histogram. Is the window big enough??\n");
+      //ExitProgram (EXIT_FAILURE);
+   }
+
+
 #define BAR(a,b,c) A_BOLD, a, b, c, ' '|A_REVERSE|COLOR_PAIR(3), Box
 
    /* Set the histogram values. */
-   setCDKHistogram (volume, vPERCENT, CENTER, BAR (0, 10, 6));
-   setCDKHistogram (bass, vPERCENT, CENTER, BAR (0, 10, 3));
-   setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 7));
+   setCDKHistogram (treble, vNONE, CENTER, BAR (0, 10, 7));
+   setCDKHistogramLLChar(treble, 'A'|A_REVERSE);
    refreshCDKScreen (cdkscreen);
    sleep (4);
 
-   /* Set the histogram values. */
-   setCDKHistogram (volume, vPERCENT, CENTER, BAR (0, 10, 8));
-   setCDKHistogram (bass, vPERCENT, CENTER, BAR (0, 10, 1));
-   setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 9));
+   
+   setCDKHistogram (Other, vNONE, CENTER, BAR (0, 10, 4));
    refreshCDKScreen (cdkscreen);
    sleep (4);
+   /* Set the histogram values. */
+   //setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 9));
+   //refreshCDKScreen (cdkscreen);
+   //sleep (4);
 
    /* Set the histogram values. */
-   setCDKHistogram (volume, vPERCENT, CENTER, BAR (0, 10, 10));
-   setCDKHistogram (bass, vPERCENT, CENTER, BAR (0, 10, 7));
-   setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 10));
-   refreshCDKScreen (cdkscreen);
-   sleep (4);
+   //setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 10));
+   //refreshCDKScreen (cdkscreen);
+   //sleep (4);
 
    /* Set the histogram values. */
-   setCDKHistogram (volume, vPERCENT, CENTER, BAR (0, 10, 1));
-   setCDKHistogram (bass, vPERCENT, CENTER, BAR (0, 10, 8));
-   setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 3));
-   refreshCDKScreen (cdkscreen);
-   sleep (4);
+   //setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 3));
+   //refreshCDKScreen (cdkscreen);
+   //sleep (4);
 
    /* Set the histogram values. */
-   setCDKHistogram (volume, vPERCENT, CENTER, BAR (0, 10, 3));
-   setCDKHistogram (bass, vPERCENT, CENTER, BAR (0, 10, 3));
-   setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 3));
-   refreshCDKScreen (cdkscreen);
-   sleep (4);
+   //setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 3));
+   //refreshCDKScreen (cdkscreen);
+   //sleep (4);
 
    /* Set the histogram values. */
-   setCDKHistogram (volume, vPERCENT, CENTER, BAR (0, 10, 10));
-   setCDKHistogram (bass, vPERCENT, CENTER, BAR (0, 10, 10));
-   setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 10));
-   refreshCDKScreen (cdkscreen);
-   sleep (4);
+   //setCDKHistogram (treble, vPERCENT, CENTER, BAR (0, 10, 10));
+   //refreshCDKScreen (cdkscreen);
+   //sleep (4);
 
    /* Clean up. */
-   destroyCDKHistogram (volume);
-   destroyCDKHistogram (bass);
    destroyCDKHistogram (treble);
    destroyCDKScreen (cdkscreen);
    endCDK ();
